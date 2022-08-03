06783ee582f23dee0b0d06eac1422eb40a26332c
xalvlax <sergiosalvatierra86@gmail.com>
2022-08-03 04:37:21 -0300

    Funcionando al terminar dia

--- ProyectoGrupalMTV/AppWebGrupal/admin.py
+++ ProyectoGrupalMTV/AppWebGrupal/admin.py
@@ -1,3 +1,13 @@
-from django.contrib import admin
+from django.contrib import admin 
+from .models import Empleados, Tareas, Herramientas
 
 # Register your models here.
+
+class EmpleadosAdmin(admin.ModelAdmin):
+    list_display = ['apellido', 'nombre', 'edad', 'ocupacion']
+    search_fields = ['apellido', 'nombre']
+
+# Register your models here.
+admin.site.register(Empleados, EmpleadosAdmin)
+admin.site.register(Tareas)
+admin.site.register(Herramientas)
--- ProyectoGrupalMTV/AppWebGrupal/models.py
+++ ProyectoGrupalMTV/AppWebGrupal/models.py
@@ -1,27 +1,26 @@
 from django.db import models
 
 # Create your models here.
-class Empleado(models.Model):
-    nombre = models.CharField(max_length=30)
+class Empleados(models.Model):
     apellido = models.CharField(max_length=30)
+    nombre = models.CharField(max_length=30)
     edad = models.IntegerField()
-    fechaNacimiento = models.DateField()
     ocupacion = models.CharField(max_length=30)
     class Meta():
         ordering = ('apellido', 'nombre') # Ordena.
-        unique_together = ('nombre', 'apellido', 'edad', 'fechaNacimiento') # No permite que se repitan.
+        unique_together = ('apellido', 'nombre', 'edad') # No permite que se repitan.
         verbose_name = 'Empleado'
         verbose_name_plural = 'Empleados'
 
     def __str__(self) -> str:
-       return f'{self.apellido}, {self.nombre}'
+       return f'{self.apellido} {self.nombre}'
     # Muestra en la pagina del Admin "apellido, nombre"
 
 
 class Tareas(models.Model):
+    responsable = models.ForeignKey(Empleados, on_delete=models.CASCADE, default=True, blank=True)
     nombre = models.CharField(max_length=30) 
-    dia_de_creacion = models.DateTimeField(auto_now_add=True)
-    responsable = models.ForeignKey(Empleado, on_delete=models.CASCADE)
+    dia_de_creacion = models.DateField(auto_now_add=True)
 
     class Meta():
         ordering = ('dia_de_creacion', '-nombre', 'responsable') # Ordena.
@@ -29,17 +28,17 @@ class Tareas(models.Model):
         verbose_name_plural = 'Tareas'
 
     def __str__(self) -> str:
-       return f'{self.dia_de_creacion}, {self.nombre}'
+       return f'{self.nombre} - {self.dia_de_creacion}'
     # Muestra en la pagina del Admin "apellido, nombre"
 
 
 class Herramientas(models.Model):
+    tipo_de_tarea = models.ForeignKey(Tareas, on_delete=models.CASCADE, default=True, blank=True)
     nombre = models.CharField(max_length=30)
-    disponible =  models.BooleanField()
-    tipo_de_tarea = models.ForeignKey(Tareas, on_delete=models.CASCADE)
+    disponible =  models.BooleanField(default=False)
 
     class Meta():
-        ordering = ('nombre', 'disponible', 'tipo_de_tarea') # Ordena.
+        ordering = ('nombre', 'disponible') # Ordena.
         verbose_name = 'Herramienta'
         verbose_name_plural = 'Herramientas'
 
